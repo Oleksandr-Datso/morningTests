7bc75087ea8c6634b23a602172a6a73c4548f0a5
Oleksandr Datso <oleksandr.datso@intellias.com>
2024-03-03 20:44:19 +0200

    upd test

--- swaggerTests/testWithBooks.js
+++ swaggerTests/testWithBooks.js
@@ -9,7 +9,6 @@ const DataClass = require('./data');
 const {testUserData} = require('./credentials');
 const HelpingFunctions = require('./helper');
 
-
 let bookStoreApi = new BookStoreApi(process.env.HOST); //тут ловим то, что передали в командной строке HOST="demoqa.com" npm run ....
 let accountApi = new AccountApi(process.env.HOST);
 let helpingFunctions = new HelpingFunctions();
